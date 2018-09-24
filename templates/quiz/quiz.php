2cfa22f69a3fc63317193916c8f688d1d5a5eccb
VanyokB <43088491+VanyokB@users.noreply.github.com>
2018-09-24 15:56:37 +0300

    tot azi

--- templates/quiz/quiz.php
+++ templates/quiz/quiz.php
@@ -1,7 +1,7 @@
 <?php
     include'header.php';
     $numquestions = $quiz->countQuestions();
-    $leadersToShow = 10;
+    // $leadersToShow = 10;
 ?>
     <div id="container" class="quiz">
       <div class="row">
@@ -10,11 +10,30 @@
                   .'</div>'; } ?>
           <h2><?php echo $quiz->getName(); ?></h2>
           <h4><?php echo $quiz->getDescription(); ?></h4>
-          <p>După ce ați început testul, trebuie să răspundeți la toate întrebările. Dacă încercați să reveniți la întrebarea anterioară, scorul dvs. va fi resetat și veți fi trimis înapoi la început.</p>
-          <p>Pentru fiecare întrebare  dati click pe <strong>'Trimite Raspuns'</strong>. Vei primi o intrebare noua.</p>
+          <p>După ce ați început testul, încercați să răspundeți la toate întrebările.</p>
+          <!-- <p>Pentru fiecare întrebare  dati click pe <strong>'Trimite Raspuns'</strong>. Vei primi o intrebare noua.</p> -->
           <p>Acest test are <strong><?php echo $numquestions; ?></strong> întrebări.</p>
           <p>Rezultatul îl vei vedea la sfîrșitul testului.</p>
-          <div id="leaders-score">
+         <!-- leaders-score start-->
+
+         <!-- leaders-score-->
+              <div>
+                  <form id="questionBox" method="post" action="<?php echo $root; ?>/quiz/process">
+                      <p>
+                          <input type="hidden" name="quizid" value="<?php echo $quiz->getId(); ?>" />
+                          <input type="submit" id="submitstart" name="starter" class="btn btn-lg btn-success"
+                          value="Începe Exercițiul" />
+                      </p>
+                  </form>
+              </div>
+          </div>
+        
+      </div><!-- /.row -->
+        
+    </div><!--container-->
+<?php include 'footer.php';
+
+/* <div id="leaders-score">
                 <h4>Scoruri de Top <?php echo $leadersToShow; ?></h4>
                 <div class="row">
                     <ul class="leaders col-md-6">
@@ -36,19 +55,5 @@
                         ?>
                     </ul>
                 </div>
-            </div><!-- leaders-score-->
-              <div>
-                  <form id="questionBox" method="post" action="<?php echo $root; ?>/quiz/process">
-                      <p>
-                          <input type="hidden" name="quizid" value="<?php echo $quiz->getId(); ?>" />
-                          <input type="submit" id="submitstart" name="starter" class="btn btn-lg btn-success"
-                          value="Start Quiz" />
-                      </p>
-                  </form>
-              </div>
-          </div>
-        
-      </div><!-- /.row -->
-        
-    </div><!--container-->
-<?php include 'footer.php';
+            </div>
+*/
